5b442f6ecf47023753139a7de06f52f0f38e3560
rachitdBeast <81420042+rachitdBeast@users.noreply.github.com>
2021-12-02 01:34:44 +0530

    Add files via upload

---
+++ 3_Implementation/inc/code.h
@@ -0,0 +1,11 @@
+#include <avr/io.h>
+#define F_CPU 1000000
+#include <util/delay.h>
+#include <stdlib.h>
+
+#define enable            5
+#define registerselection 6
+
+void send_a_command(unsigned char command);
+void send_a_character(unsigned char character);
+void send_a_string(char *string_of_characters);
