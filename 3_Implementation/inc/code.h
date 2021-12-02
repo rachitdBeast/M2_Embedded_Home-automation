1fcd8fbe287a6420f99308044906beedc222ab6f
rachitdBeast <81420042+rachitdBeast@users.noreply.github.com>
2021-12-02 11:45:46 +0530

    Update code.h

--- 3_Implementation/inc/code.h
+++ 3_Implementation/inc/code.h
@@ -1,3 +1,13 @@
+/**
+ * @file code.h
+ * @author Selwyn Rachit Bhengra (selwynrachit@karunya.edu.in)
+ * @brief 
+ * @version 0.1
+ * @date 2021-12-02
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include <avr/io.h>
 #define F_CPU 1000000
 #include <util/delay.h>
